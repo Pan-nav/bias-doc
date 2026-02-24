97ee3e84a96a03ccd71de15230a44a768e27be26
Panav Bindal <102317251+Pan-nav@users.noreply.github.com>
2026-02-24 00:16:30 +0000

    Changed feature card colour

--- src/lib/components/FeatureCard.svelte
+++ src/lib/components/FeatureCard.svelte
@@ -15,7 +15,7 @@
 </script>
 
 <div
-	class="flex h-[60px] w-[210px] items-center justify-center rounded-[15px] bg-[#070707] font-inter text-[15px] font-bold text-white"
+	class="flex h-[60px] w-[210px] items-center justify-center rounded-[15px] bg-[#131313] font-inter text-[15px] font-bold text-white"
 	in:fly={{ y: 16, duration: 350 }}
 >
 	{title}
